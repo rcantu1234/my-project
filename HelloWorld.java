5d40aac043f4bd4ddd1357900e1778c046d44596
roelcantu <roelhcantu@gmail.com>
2019-11-08 09:05:10 -0600

    Implemented the go()

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
+  public static final String java = "Go Java";
+
   public static void main(String[] args) {
     System.out.println("Hello World!!!");
+  }
+
+  public static void go() {
+    System.out.println(java);
   }
 }
