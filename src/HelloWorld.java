7135d5d0694a147b06e319c0fc7d229c7406f9b9
roelcantu <roelhcantu@gmail.com>
2019-11-08 10:13:54 -0600

    ADDED MORE COMMENTS

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
   // Adding comments for GIT demo.
   public static final String java = "Go Java";
 
+  // Adding more comments for GIT Demo.
   public static void main(String[] args) {
     System.out.println("Hello World!!!");
   }
